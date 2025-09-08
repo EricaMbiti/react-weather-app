bb8bed54baf62d2abbb3bd61e3921e0f45cb9275
EricaMbiti <mbitierica@gmail.com>
2025-09-08 15:35:27 +0200

    Added bootstrap and updated dependencies

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="Container">
+     <h1> Weather App</h1>
 <footer>
   This project is coded by Erica Mbiti, and it is open sourced on {""}
 <a href="https://github.com/EricaMbiti/react-weather-app" target="_blank" rel="noopener noreferrer" >Github
 </a>
 </footer>
+</div>
     </div>
   );
 }
