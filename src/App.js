39a11684180ad700e84b71d5a98aee8513ff7623
EricaMbiti <mbitierica@gmail.com>
2025-09-08 15:06:01 +0200

    Added Github repo Url and a footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+<footer>
+  This project is coded by Erica Mbiti, and it is open sourced on {""}
+<a href="https://github.com/EricaMbiti/react-weather-app" target="_blank" rel="noopener noreferrer" >Github
+</a>
+</footer>
     </div>
   );
 }
