ce86aefd52880066ec081a43dda5a401ff53cc3e
EricaMbiti <mbitierica@gmail.com>
2025-09-13 16:22:24 +0200

    added a component that allows changing of units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTempUnit from "./WeatherTempUnit";
 
 export default function WeatherInfo(props) {
     return ( 
@@ -22,10 +23,8 @@ export default function WeatherInfo(props) {
 <WeatherIcon code={props.data.icon} />
 </div>
 
+ <WeatherTempUnit celsius={props.data.temperature} />
 
-<span className="temperature">{Math.round(props.data.temperature)}</span>
-
- <span className="unit">°C</span>
 
 </div>
 </div>
